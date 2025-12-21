99879cec44130e4d5ee2fd17b5fcc3c27b1dd50c
artem <jock.fromrussia@gmail.com>
2025-12-21 09:29:31 +0300

    максону на рефакторинг

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://localhost:8080',
+  baseURL: 'http://localhost:8080',
 });
 
 api.interceptors.request.use(config => {
-	if (config.skipAuth) {
-		delete config.skipAuth;
-		return config;
-	}
+  if (config.skipAuth) {
+    delete config.skipAuth;
+    return config;
+  }
 
-	const token = localStorage.getItem('token');
-	config.headers.Authorization = `Bearer ${token}`;
-	return config;
+  const token = localStorage.getItem('token');
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default api;
 
 api.interceptors.response.use(
-	response => response,
-	error => {
-		if (error.request && !error.response) error.isNetworkError = true;
-		return Promise.reject(error);
-	}
+  response => response,
+  error => {
+    if (error.request && !error.response) error.isNetworkError = true;
+    return Promise.reject(error);
+  }
 );
 

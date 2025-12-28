cad044ab8d6639742066246d05dbcf554cc21e27
artem <jock.fromrussia@gmail.com>
2025-12-28 12:33:18 +0300

    закончил делать профиль

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,41 +1,41 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
-import {getUserThunk} from "../app/features/users/usersThunk.js";
-import {getTimeThunk} from "../app/features/time/timeThunk.js";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getUserThunk } from '../app/features/users/usersThunk.js';
+import { getTimeThunk } from '../app/features/time/timeThunk.js';
 
 const Layout = () => {
-  const status = useSelector(state => state.users.status);
+	const status = useSelector((state) => state.users.status);
 
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (status === 'inactive') dispatch(getUserThunk());
-  }, [status, dispatch]);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (status === 'inactive') dispatch(getUserThunk());
+	}, [status, dispatch]);
 
-  useEffect(() => {
-    if (status === 'inactive') {
-      dispatch(getTimeThunk());
-    }
-  }, [dispatch, status]);
+	useEffect(() => {
+		if (status === 'inactive') {
+			dispatch(getTimeThunk());
+		}
+	}, [dispatch, status]);
 
-  let navData = [
-    { to: '/', text: 'НОВОСТИ' },
-    { to: '/auth', text: 'ВОЙТИ В АККАУНТ' },
-  ];
+	let navData = [
+		{ to: '/', text: 'НОВОСТИ' },
+		{ to: '/auth', text: 'ВОЙТИ В АККАУНТ' },
+	];
 
-  if (status === 'success') {
-    navData = [
-      {to: '/', text: 'НОВОСТИ'},
-      {to: '/categories', text: 'КАТАЛОГ'},
-      {to: '/profile', text: 'ПРОФИЛЬ'}
-    ];
-  }
+	if (localStorage.getItem('token')) {
+		navData = [
+			{ to: '/', text: 'НОВОСТИ' },
+			{ to: '/categories', text: 'КАТАЛОГ' },
+			{ to: '/profile', text: 'ПРОФИЛЬ' },
+		];
+	}
 
 	return (
 		<>
 			<header>
-        <Navbar data={navData} />
+				<Navbar data={navData} />
 			</header>
 
 			<main>
@@ -46,6 +46,3 @@ const Layout = () => {
 };
 
 export default Layout;
-
-
-
